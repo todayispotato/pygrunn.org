62cd21425d012a5f7ae1563d968741fb272eb073
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2014-02-12 21:54:01 +0100

    Updated sponsors text. Created sponsors section in top. Social buttons.

--- static/javascript/application.js
+++ static/javascript/application.js
@@ -1,39 +1,44 @@
 jQuery(function($) {
 
-    var fn = function(){},
-        win = $(window),
-        fullscreenPos = $('.fullscreen'),
-        navToggle = $('.uk-navbar-toggle'),
-        offcanvasMenu = $('#offcanvasmenu'),
-        offcanvasMenuLink = $('.uk-offcanvasmenu-bar li > a');
-
-    fn = function(){
-
-        setTimeout(function(){ fullscreenPos.css('height', win.height()); }, 10);
-
+	var fn = function(){},
+		win = $(window),
+		fullscreenPos = $('.fullscreen'),
+		offcanvasMenu = $('#offcanvasmenu'),
+		offcanvasMenuLink = $('.uk-offcanvasmenu-bar li > a'),
+		logo = $('.uk-logo');
+		
+	fn = function(){
+	
+		setTimeout(function(){ fullscreenPos.css('height', win.height()); }, 10);
+		
     };
-
-    win.on('resize', function(){
+	
+	win.on('resize', function(){
 
         fn();
 
         return fn;
 
     }());
-
-    offcanvasMenuLink.on('click', function(){
-
-        if (!offcanvasMenu.hasClass('uk-hidden')) {
-
-            offcanvasMenu.addClass('uk-hidden');
-
-        }
-
-    });
-
-    $('#slides').slidesjs({
-        navigation: false,
-        pagination: false
-    });
+	
+	offcanvasMenuLink.on('click', function(){
+		
+		if (!offcanvasMenu.hasClass('uk-hidden')) {
+		
+			offcanvasMenu.addClass('uk-hidden');
+		
+		}
+		
+	});
+	
+	logo.on('click', function(){
+		
+		if (!offcanvasMenu.hasClass('uk-hidden')) {
+		
+			offcanvasMenu.addClass('uk-hidden');
+		
+		}
+		
+	});
 
 });
