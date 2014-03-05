68d702dce833a625f839be2cf87292347d98250c
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2014-03-05 20:51:16 +0100

    Added about section with former speakers and photos of PyGrunn 2013.

--- static/javascript/application.js
+++ static/javascript/application.js
@@ -1,57 +1,57 @@
 jQuery(function($) {
 
-	var fn = function(){},
-		win = $(window),
-		fullscreenPos = $('.fullscreen'),
-		offcanvasMenu = $('#offcanvasmenu'),
-		offcanvasMenuLink = $('.uk-offcanvasmenu-bar li > a'),
-		logo = $('.uk-logo');
-		
-	fn = function(){
-	
-		setTimeout(function(){ fullscreenPos.css('height', win.height()); }, 10);
-		
+    var fn = function(){},
+        win = $(window),
+        fullscreenPos = $('.fullscreen'),
+        offcanvasMenu = $('#offcanvasmenu'),
+        offcanvasMenuLink = $('.uk-offcanvasmenu-bar li > a'),
+        logo = $('.uk-logo');
+
+    fn = function(){
+
+        setTimeout(function(){ fullscreenPos.css('height', win.height()); }, 10);
+
     };
-	
-	win.on('load resize', (function(){
+
+    win.on('load resize', (function(){
 
         fn();
 
         return fn;
 
     })());
-	
-	var hideOffCanvasMenu = function(){
-		
-		if (!offcanvasMenu.hasClass('uk-hidden')) {
-		
-			offcanvasMenu.addClass('uk-hidden');
-		
-		}
-		
-	};
-	
-	offcanvasMenuLink.on('click', function(){
-		
-		hideOffCanvasMenu();
-		
-	});
-	
-	logo.on('click', function(){
-		
-		hideOffCanvasMenu();
-		
-		$('html,body').stop().animate({ scrollTop: 0 }, 600, 'easeOutExpo');
-		
-	});
-	
-	$(document).ready(function() {
-	
-		$('.fancybox').fancybox({
-			padding: 1,
-			margin: 30
-		});
-		
-	});
+
+    var hideOffCanvasMenu = function(){
+
+        if (!offcanvasMenu.hasClass('uk-hidden')) {
+
+            offcanvasMenu.addClass('uk-hidden');
+
+        }
+
+    };
+
+    offcanvasMenuLink.on('click', function(){
+
+        hideOffCanvasMenu();
+
+    });
+
+    logo.on('click', function(){
+
+        hideOffCanvasMenu();
+
+        $('html,body').stop().animate({ scrollTop: 0 }, 600, 'easeOutExpo');
+
+    });
+
+    $(document).ready(function() {
+
+        $('.fancybox').fancybox({
+            padding: 1,
+            margin: 30
+        });
+
+    });
 
 });
